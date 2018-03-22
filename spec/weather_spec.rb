21836893e94ef443dacecfca0a0e21653c5b21dd
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-22 22:58:21 +0000

    5 tests added

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -9,8 +9,29 @@ describe Weatherio do
       @response = @weatherio.weather_service.weather_response('bogota')
     end
 
-    it "should respond with a status message of 200" do
+    it "Longitude and Latitude should be Integers" do
       expect(@response["coord"]["lon"]).to be_a(Float)
+      expect(@response["coord"]["lat"]).to be_a(Float)
     end
+
+    it "Weather Description should be a string" do
+      expect(@response["weather"].first["description"]).to be_a(String)
+    end
+
+    it "Country shortened name should be up to 3 letters long" do
+      expect(@response["sys"]["country"].length).to be_between(1,3)
+    end
+
+    it "Country name should be a string" do
+      expect(@response["name"]).to be_a(String)
+    end
+
+    it "Temperature should be a decimal" do
+      expect(@response["main"]["temp"]).to be_a(Float)
+      expect(@response["main"]["temp_min"]).to be_a(Float)
+      expect(@response["main"]["temp_max"]).to be_a(Float)
+    end
+
+
   end
 end
