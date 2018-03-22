1d412b3082d39b848bda6f907e246d60d456f20f
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-22 21:19:45 +0000

    api added'

---
+++ lib/weather.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+require 'json'
+require_relative "./services/weather_service"
+
+class Weatherio
+  include HTTParty
+
+  base_uri 'http://api.openweathermap.org/data/2.5'
+
+  def weather_service
+    WeatherService.new
+
+  end
+end
+
+x = Weatherio.new
+
+print x.weather_service
