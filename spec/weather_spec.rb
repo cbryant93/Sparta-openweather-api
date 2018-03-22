404ae7facb2c14c18942a57a8ade7d1cdc7c22c4
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-22 22:17:15 +0000

    first test successfully added

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -8,5 +8,9 @@ describe Weatherio do
       @weatherio = Weatherio.new
       @response = @weatherio.weather_service.weather_response('bogota')
     end
+
+    it "should respond with a status message of 200" do
+      expect(@response["coord"]["lon"]).to be_a(Float)
+    end
   end
 end
