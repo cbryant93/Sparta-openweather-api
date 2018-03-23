509b28b9228c70aa4c79d19a1dce119fd18b097e
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-23 09:22:39 +0000

    removed data file

--- city_info.rb
+++
@@ -1,9 +0,0 @@
-{"coord"=>{"lon"=>-74.08, "lat"=>4.6}, "weather"=>[{"id"=>802, "
-main"=>"Clouds", "description"=>"scattered clouds", "icon"=>"03d
-"}], "base"=>"stations", "main"=>{"temp"=>291.15, "pressure"=>10
-24, "humidity"=>63, "temp_min"=>291.15, "temp_max"=>291.15}, "vi
-
-sibility"=>10000, "wind"=>{"speed"=>5.7, "deg"=>250}, "clouds"=>
-{"all"=>40}, "dt"=>1521752400, "sys"=>{"type"=>1, "id"=>4245, "m
-essage"=>0.0037, "country"=>"CO", "sunrise"=>1521716354, "sunset
-"=>1521759991}, "id"=>3688689, "name"=>"Bogota", "cod"=>200
